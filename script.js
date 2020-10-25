a116677845da6fa2e6e19681aafa5e84bee7728e
Larissa Midori <70383649+LarissaMidori@users.noreply.github.com>
2020-10-25 00:09:14 -0300

    Joguinho terminado

--- script.js
+++ script.js
@@ -13,13 +13,13 @@ let food = {
 }
 
 function criarBG(){
-    context.fillStyle = "lightgreen";
+    context.fillStyle = "limegreen";
     context.fillRect (0, 0, 16 * box, 16 * box);
 }
 
 function criarCobrinha(){
-    for(i=0; i < snake.length; i++) {
-        context.fillStyle = "blue";
+    for(i = 0; i < snake.length; i++) {
+        context.fillStyle = "darkblue";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
@@ -47,6 +47,13 @@ function iniciarJogo(){
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
 
+    for(i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert('GAME OVER =/')
+        }
+    }
+
     criarBG();
     criarCobrinha();
     drawFood();
@@ -59,7 +66,13 @@ function iniciarJogo(){
     if(direction == "up") snakeY -= box;
     if(direction == "down") snakeY += box;
 
-    snake.pop();
+    if(snakeX != food.x || snakeY != food.y) {
+        snake.pop();
+    } else {
+        food.x = Math.floor(Math.random() * 15 + 1) * box;
+        food.y = Math.floor(Math.random() * 15 + 1) * box;
+    }
+
 
     let newHead = {
         x: snakeX,
