4714810807321433ad8879cd827766aa9ab2364a
Larissa Midori <70383649+LarissaMidori@users.noreply.github.com>
2020-10-24 23:12:54 -0300

    Criando a comidinha da cobrinha

--- script.js
+++ script.js
@@ -7,22 +7,31 @@ snake[0] = {
     y: 8 * box
 }
 let direction = "right";
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 
-function criarBG() {
+function criarBG(){
     context.fillStyle = "lightgreen";
     context.fillRect (0, 0, 16 * box, 16 * box);
 }
 
-function criarCobrinha() {
+function criarCobrinha(){
     for(i=0; i < snake.length; i++) {
         context.fillStyle = "blue";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
 
+function drawFood(){
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
+}
+
 document.addEventListener('keydown', update);
 
-function update(event) {
+function update(event){
 
  //37 == left, 39 == right, 38 == up, 40 == down
     if(event.keyCode == 37 && direction != "right") direction = "left";
@@ -32,7 +41,7 @@ function update(event) {
 
 }
 
-function iniciarJogo() {
+function iniciarJogo(){
     if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
     if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
@@ -40,6 +49,7 @@ function iniciarJogo() {
 
     criarBG();
     criarCobrinha();
+    drawFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
